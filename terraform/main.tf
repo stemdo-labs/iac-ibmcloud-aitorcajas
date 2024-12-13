766549445da11e5344bff0ed90675e425c4a36f4
aitorcajas <acajas@stemdo.io>
2024-12-13 14:34:14 +0100

    Prueba de despliegue de VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "ibm_security_group_rule" "allow_port_22" {
     port_min  = 22
     port_max  = 22
     remote_ip = "0.0.0.0/0"
-    security_group_id = ibm_security_group.allow_port_22.id
+    security_group_id = ibm_security_group.sg_vm.id
 }
 
 resource "ibm_is_instance" "vm_bd" {
