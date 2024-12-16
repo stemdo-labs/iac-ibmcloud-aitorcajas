0e1f052c23d0d1e2f2965ad9945cf7125f10a86e
aitorcajas <acajas@stemdo.io>
2024-12-16 14:03:01 +0100

    Prueba de despliegue cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,12 +101,23 @@ resource "ibm_is_subnet" "subnet_cluster" {
   ipv4_cidr_block = "10.242.0.0/24"
 }
 
-resource "ibm_container_cluster" "cluster" {
-  name              = "cluster-acajas"
-  datacenter        = "lon02"
-  hardware          = "shared"
-  machine_type      = "u2c.2x4"
-  subnet_id         = [ibm_is_subnet.subnet_cluster.id]
-  default_pool_size = 1
+resource "ibm_resource_instance" "cos_instance" {
+  name     = "acajas-cos-instance"
+  service  = "cloud-object-storage"
+  plan     = "lite"
+  location = var.location
+}
+
+resource "ibm_container_vpc_cluster" "cluster" {
+  name              = "acajas-vpc-cluster"
+  vpc_id            = ibm_is_vpc.vpc_cluster.id
+  kube_version      = "4.3_openshift"
+  flavor            = "bx2.4x16"
+  worker_count      = "1"
+  cos_instance_crn  = ibm_resource_instance.cos_instance.id
   resource_group_id = var.rg_id
+  zones {
+      subnet_id = ibm_is_subnet.subnet_cluster.id
+      name      = var.zone
+    }
 }
