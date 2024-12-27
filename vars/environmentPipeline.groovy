30a3afd959eb05e12ee799f2d054cbade7908ab7
aitorcajas <acajas@stemdo.io>
2024-12-27 13:19:20 +0100

    Librería compartida con parámetros

--- vars/environmentPipeline.groovy
+++ vars/environmentPipeline.groovy
@@ -1,48 +1,19 @@
-def call(Map pipelineParams) {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Checkout del Repositorio') {
-                steps {
-                    checkout scm
-                }
-            }
-
-            stage('Definir Entorno') {
-                steps {
-                    script {
-                        def branch = env.GIT_BRANCH
-                        echo "Rama actual: ${branch}"
-                        if (branch == 'origin/main') {
-                            env.ENVIRONMENT = 'production'
-                        } else if (branch == 'origin/develop') {
-                            env.ENVIRONMENT = 'development'
-                        }
-                        echo "Entorno definido: ${env.ENVIRONMENT}"
-                        return env.ENVIRONMENT
-                    }
-                }
-            }
-
-            // stage('Extraer Desarrollo') {
-            //     steps {
-            //         script {
-            //             def pipelineName = sh(script: "basename \$(git rev-parse --show-toplevel)", returnStdout: true).trim()
-            //             echo "Nombre de la pipeline: ${pipelineName}"
-            //             if (pipelineName.contains("backend") && env.ENVIRONMENT == 'production') {
-            //                 env.DESARROLLO = 'backend-production'
-            //             } else if (pipelineName.contains("backend") && env.ENVIRONMENT == 'development') {
-            //                 env.DESARROLLO = 'backend-development'
-            //             } else if (pipelineName.contains("frontend") && env.ENVIRONMENT == 'production') {
-            //                 env.DESARROLLO = 'frontend-production'
-            //             } else if (pipelineName.contains("frontend") && env.ENVIRONMENT == 'development') {
-            //                 env.DESARROLLO = 'frontend-development'
-            //             }
-            //             echo "Desarrollo configurado: ${env.DESARROLLO}"
-            //         }
-            //     }
-            // }
-        }
+// vars/environmentPipeline.groovy
+def call() {
+    // Obtener la rama y definir el entorno
+    def branch = env.BRANCH_NAME ?: 'main' // Si no hay BRANCH_NAME, se asume 'main'
+    
+    def entorno = ''
+    if (branch == 'main') {
+        entorno = 'production'
+    } else if (branch == 'develop') {
+        entorno = 'development'
+    } else {
+        entorno = 'unknown'
     }
+
+    // Establecer el entorno como una variable de entorno
+    env.ENVIRONMENT = entorno
+    echo "Entorno definido: ${env.ENVIRONMENT}"
+    return env.ENVIRONMENT
 }
