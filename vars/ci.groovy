995536c23ade79477cdd10c0b64c34f8135696eb
aitorcajas <acajas@stemdo.io>
2024-12-27 13:03:58 +0100

    Librería compartida con parámetros

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,12 +6,14 @@ def call(String entorno) {
         stages {
             stage('Definir Entorno') {
                 steps {
+                    script {
                     echo "El entorno es: ${entorno}"
                     // Lógica para ejecutar según el entorno
                     if (entorno == 'production') {
                         echo "Ejecutando en producción"
                     } else if (entorno == 'development') {
                         echo "Ejecutando en desarrollo"
+                    }
                     }
                 }
             }
