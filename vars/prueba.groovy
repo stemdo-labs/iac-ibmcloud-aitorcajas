1e3101711610c3a2870f197e1280ea6cb085039d
aitorcajas <acajas@stemdo.io>
2024-12-27 12:55:37 +0100

    Librería compartida con parámetros

--- vars/prueba.groovy
+++ vars/prueba.groovy
@@ -1,8 +1,3 @@
 def call() {
-    if (env.GIT_BRANCH == 'origin/main') {
-        env.ENVIRONMENT = 'production'
-    } else if (env.GIT_BRANCH == 'origin/develop') {
-        env.ENVIRONMENT = 'development'
-    }
-    return env.ENVIRONMENT
+    return 'development'
 }
