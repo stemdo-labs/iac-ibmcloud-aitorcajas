84dc9d76d64b8029c5c8775fc4df3956b8e0c203
aitorcajas <acajas@stemdo.io>
2024-12-30 09:56:31 +0100

    Cambio a scripted pipeline

--- vars/environmentPipeline.groovy
+++ vars/environmentPipeline.groovy
@@ -1,42 +1,36 @@
 def ejecutarPipeline() {
     node {
         stage('Checkout del Repositorio') {
-            steps {
-                checkout scm
-            }
+            checkout scm
         }
 
         stage('Definir Entorno') {
-            steps {
-                script {
-                    def branch = env.GIT_BRANCH
-                    echo "Rama actual: ${branch}"
-                    if (branch == 'origin/main') {
-                        env.ENVIRONMENT = 'production'
-                    } else if (branch == 'origin/develop') {
-                        env.ENVIRONMENT = 'development'
-                    }
-                    echo "Entorno definido: ${env.ENVIRONMENT}"
+            script {
+                def branch = env.GIT_BRANCH
+                echo "Rama actual: ${branch}"
+                if (branch == 'origin/main') {
+                    env.ENVIRONMENT = 'production'
+                } else if (branch == 'origin/develop') {
+                    env.ENVIRONMENT = 'development'
                 }
+                echo "Entorno definido: ${env.ENVIRONMENT}"
             }
         }
 
         stage('Extraer Desarrollo') {
-            steps {
-                script {
-                    def pipelineName = sh(script: "basename \$(git rev-parse --show-toplevel)", returnStdout: true).trim()
-                    echo "Nombre de la pipeline: ${pipelineName}"
-                    if (pipelineName.contains("backend") && env.ENVIRONMENT == 'production') {
-                        env.DESARROLLO = 'backend-production'
-                    } else if (pipelineName.contains("backend") && env.ENVIRONMENT == 'development') {
-                        env.DESARROLLO = 'backend-development'
-                    } else if (pipelineName.contains("frontend") && env.ENVIRONMENT == 'production') {
-                        env.DESARROLLO = 'frontend-production'
-                    } else if (pipelineName.contains("frontend") && env.ENVIRONMENT == 'development') {
-                        env.DESARROLLO = 'frontend-development'
-                    }
-                    echo "Desarrollo configurado: ${env.DESARROLLO}"
+            script {
+                def pipelineName = sh(script: "basename \$(git rev-parse --show-toplevel)", returnStdout: true).trim()
+                echo "Nombre de la pipeline: ${pipelineName}"
+                if (pipelineName.contains("backend") && env.ENVIRONMENT == 'production') {
+                    env.DESARROLLO = 'backend-production'
+                } else if (pipelineName.contains("backend") && env.ENVIRONMENT == 'development') {
+                    env.DESARROLLO = 'backend-development'
+                } else if (pipelineName.contains("frontend") && env.ENVIRONMENT == 'production') {
+                    env.DESARROLLO = 'frontend-production'
+                } else if (pipelineName.contains("frontend") && env.ENVIRONMENT == 'development') {
+                    env.DESARROLLO = 'frontend-development'
                 }
+                echo "Desarrollo configurado: ${env.DESARROLLO}"
             }
         }
     }
