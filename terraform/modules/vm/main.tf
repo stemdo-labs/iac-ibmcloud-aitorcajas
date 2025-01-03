3af6d486bfd1a293994a47bd250d4b436808499b
aitorcajas <acajas@stemdo.io>
2025-01-03 08:15:37 +0100

    Cambio de nombre ssh key

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 resource "ibm_is_ssh_key" "ssh_key_vm" {
-  name           = "public-ssh-key"
+  name           = "acajas-ssh-key"
   public_key     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/yhO+AunpWhYviv0+oBd+bwRx3cAN+0SzKEC9OKdiU7Q65A+U899NgitI1Qr4tsujo7/o8qw/w707wXn8tfeWB2pTiVUJ9qap90jtaMSDwPs9VYxWDFS687vxZ8k9N0Fmws0cg8qxdhoFJJU0OcjI0mQLZmFohr1el9ZEdxW+NWDfWy22e70DIlVO4oqv05OUV8yXZDFeOxVGxWAEJt0UsQGoup8m1cSEOkbrqnmrxDD93/sYBDMHf6aemoY9bCu9tJJGNg/hAt4pPzmtz/iVgoBHLOhPIintL5xD6DU304dkvY4y7eoNorMW5BzOdxRwYlb3fibYITQv5dJALPaygEmqEOWDFlNyWbHmcM5RsjrVnRvD19sS+ZPVucqAzomp9k83Fvm0csBxObf0QiYLJuIG66a7q1Pfamj6izVG4Cc9jrTwllQdLqv3Nri9pCBvuueDNWm8B8mJp4bWYaNuWFr9PnKJ8XWU+3puoi+9SFxKoXyU+ql1C9sXiWGKHsU="
   resource_group = var.rg_id
 }
